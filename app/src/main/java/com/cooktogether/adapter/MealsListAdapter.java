091770203c4a26c8e02cdc2b80637cdc9f55b21f
Hela Othmani <hela.Othmani@ensimag.grenoble-inp.fr>
2017-01-22 13:09:34 +0100

    meal status

--- app/src/main/java/com/cooktogether/adapter/MealsListAdapter.java
+++ app/src/main/java/com/cooktogether/adapter/MealsListAdapter.java
@@ -1,9 +1,12 @@
 package com.cooktogether.adapter;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.cooktogether.R;
 import com.cooktogether.mainscreens.HomeActivity;
@@ -11,6 +14,8 @@ import com.cooktogether.model.Meal;
 import com.cooktogether.viewholder.MealViewHolder;
 
 import java.util.ArrayList;
+
+import static android.view.View.GONE;
 
 /**
  * Created by hela on 15/01/17.
@@ -31,7 +36,6 @@ public class MealsListAdapter extends RecyclerView.Adapter<MealViewHolder>{
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_meal, parent, false);
-
         MealViewHolder vMeal = new MealViewHolder(v);
 
         return vMeal;
@@ -39,7 +43,26 @@ public class MealsListAdapter extends RecyclerView.Adapter<MealViewHolder>{
 
     @Override
     public void onBindViewHolder(MealViewHolder holder, int position) {
-        holder.bindToPost(meals.get(position));
+        Meal m = meals.get(position);
+        TextView mealStatus = (TextView)holder.itemView.findViewById(R.id.meal_status);
+        ProgressBar mealRsvProgress = (ProgressBar) holder.itemView.findViewById(R.id.reservations_progress_bar);
+        TextView progressTxt = (TextView) holder.itemView.findViewById(R.id.progress_bar_txt);
+        if(m.getBooked()) {
+            mealStatus.setText("BOOKED");
+            mealStatus.setTextColor(Color.RED);
+            holder.itemView.findViewById(R.id.progress_bar_layout).setVisibility(View.GONE);
+            mealStatus.setVisibility(View.VISIBLE);
+        }
+        else{
+            mealRsvProgress.setMax(m.getNbrPersons());
+            mealRsvProgress.setProgress(m.getNbrReservations());
+            progressTxt.setText(m.getNbrReservations() +"/"+ m.getNbrPersons()+" places reserved");
+            progressTxt.setTextColor(Color.GREEN);
+            mealStatus.setVisibility(GONE);
+            holder.itemView.findViewById(R.id.progress_bar_layout).setVisibility(View.VISIBLE);
+        }
+
+        holder.bindToPost(m);
     }
 
     @Override
